a00b8e3f69961036548af023d3d900fa26424ff1
JungUiJin <yealkki38@gmail.com>
2024-08-14 18:45:28 +0900

    검색/수정 기능구현

--- main/java/com/todoList/run/Application.java
+++ main/java/com/todoList/run/Application.java
@@ -4,7 +4,6 @@ import com.todoList.aggregate.Todo;
 import com.todoList.service.TodoService;
 
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.Scanner;
 
 public class Application {
@@ -29,11 +28,26 @@ public class Application {
             int choice = sc.nextInt();
             switch (choice) {
                 case 1: todoService.selectAllTodo(chooseType());; break;
+                case 2: todoService.searchTodoList(chooseTitle()); break;
                 case 3: todoService.insertTodo(addTodo()); break;
+                case 4:
+                    Todo selectedTodo = todoService.searchOneTodo(chooseTitle());
+                    if (selectedTodo == null) { continue; }
+                    todoService.updateTodo(reform(selectedTodo));
+                    break;
                 case 9:
                     System.out.println("프로그램을 종료합니다."); return;
+                default:
+                    System.out.println("올바른 값을 입력해주세요.");
             }
         }
+    }
+
+    private static String chooseTitle() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("검색할 일정 제목을 입력하십시오. : ");
+        String title = sc.nextLine();
+        return title;
     }
 
     private static int chooseType(){
@@ -67,5 +81,47 @@ public class Application {
         newTodo = new Todo(title,content,startDate,endDate,importance);
 
         return newTodo;
+    }
+    private static Todo reform(Todo selected){
+        Todo todo = selected;
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            System.out.println("==== 일정 " +todo.getTitle()+ "수정 ====");
+            System.out.println("1. 제목 수정");
+            System.out.println("2. 내용 수정");
+            System.out.println("3. 시작일 수정");
+            System.out.println("4. 마감일 수정");
+            System.out.println("5. 중요도 수정");
+            System.out.println("9. 수정 종료");
+            int choice = sc.nextInt();
+            sc.nextLine();
+            switch (choice) {
+                case 1:
+                    System.out.print("수정할 제목을 입력하십시오. : ");
+                    todo.setTitle(sc.nextLine());
+                    break;
+                case 2:
+                    System.out.print("수정할 내용을 입력하십시오. : ");
+                    todo.setContent(sc.nextLine());
+                    break;
+                case 3:
+                    System.out.print("수정할 시작일을 입력하십시오. : ");
+                    todo.setStartDate(LocalDate.parse(sc.nextLine()));
+                    break;
+                case 4:
+                    System.out.print("수정할 마감일을 입력하십시오. : ");
+                    todo.setEndDate(LocalDate.parse(sc.nextLine()));
+                    break;
+                case 5:
+                    System.out.print("수정할 중요도을 입력하십시오. : ");
+                    todo.setImportance(sc.nextInt());
+                    break;
+                case 9:
+                    System.out.println("메인메뉴로 돌아갑니다.");
+                    return selected;
+                default:
+                    System.out.println("올바른 값을 입력해 주세요.");
+            }
+        }
     }
 }
--- main/java/com/todoList/service/TodoService.java
+++ main/java/com/todoList/service/TodoService.java
@@ -13,7 +13,6 @@ public class TodoService {
 
     private final TodoRepository todoRepository = new TodoRepository();
 
-
     public void insertTodo(Todo addTodo) {
         int lastTodoNo = todoRepository.selectLastNumber();
         addTodo.setNo(lastTodoNo+1);
@@ -26,7 +25,9 @@ public class TodoService {
 
     public void selectAllTodo(int type) {
         ArrayList<Todo> allTodoList = todoRepository.selectAllTodo();
+
         List<Todo> endList = null;
+
         switch (type){
             case 0: endList = allTodoList; break;
             case 1:
@@ -44,5 +45,38 @@ public class TodoService {
         for(Todo todo : endList) {
             System.out.println(todo);
         }
+
+    }
+
+    public void searchTodoList(String title) {
+        List<Todo> sTodo = todoRepository.selectTodo(title);
+        if (sTodo == null || sTodo.isEmpty()) {
+            System.out.println("입력하신 제목을 포함하는 일정이 없습니다.");
+        } else {
+            for (Todo todo : sTodo) {
+                System.out.println(todo);
+            }
+        }
+    }
+
+    public Todo searchOneTodo(String title) {
+        Todo todo = todoRepository.selectOneTodo(title);
+        if(todo != null){
+            todo.setNo(todo.getNo());
+            todo.setTitle(todo.getTitle());
+            todo.setContent(todo.getContent());
+            todo.setStartDate(todo.getStartDate());
+            todo.setEndDate(todo.getEndDate());
+            todo.setStatus(todo.getStatus());
+            todo.setStatus(todo.getStatus());
+
+            return todo;
+        }
+        System.out.println("입력하신 제목의 일정이 없습니다.");
+        return null;
+    }
+
+    public void updateTodo(Todo reform) {
+        todoRepository.modifyTodo(reform);
     }
 }
